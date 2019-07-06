9577f6269b8d89f38a1c80054494fba41c303654
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-07-06 10:56:05 -0500

    Simpler

--- src/test/kotlin/hm/binkley/basilisk/PersistenceSpec.kt
+++ src/test/kotlin/hm/binkley/basilisk/PersistenceSpec.kt
@@ -2,36 +2,17 @@ package hm.binkley.basilisk
 
 import ch.tutteli.atrium.api.cc.en_GB.containsExactly
 import ch.tutteli.atrium.verbs.expect
-import com.zaxxer.hikari.util.DriverDataSource
-import io.micronaut.context.annotation.Bean
-import io.micronaut.context.annotation.Replaces
 import io.micronaut.test.annotation.MicronautTest
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.testcontainers.containers.PostgreSQLContainerProvider
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import java.util.*
 
 @MicronautTest
 @TestInstance(PER_CLASS)
 @Testcontainers
 class PersistenceSpec {
-    companion object {
-        @Container
-        val container = PostgreSQLContainerProvider().newInstance()
-    }
-
-    @Bean
-    @Replaces
-    fun testDataSource() = DriverDataSource(container.getJdbcUrl(),
-            "org.postgresql.Driver",
-            Properties(),
-            container.getUsername(),
-            container.getPassword())
-
     @Test
     fun shouldRoundTripSimple() {
         transaction {
