e71591d694f1204d5fe4141feb29838f9760b1d6
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-07-14 17:08:12 -0500

    Experiment with domain

--- src/main/kotlin/hm/binkley/basilisk/Ingredient.kt
+++ src/main/kotlin/hm/binkley/basilisk/Ingredient.kt
@@ -1,9 +1,40 @@
 package hm.binkley.basilisk
 
+import io.micronaut.context.event.ApplicationEvent
+import io.micronaut.context.event.ApplicationEventPublisher
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.IntIdTable
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Ingredients(@Inject private val publisher: ApplicationEventPublisher) {
+    // TODO: Throw or return null when ID not found?
+    fun ingredient(id: Int): Ingredient {
+        val found = IngredientRecord.findById(id)
+        return Ingredient(found!!, publisher)
+    }
+}
+
+interface IngredientData {
+    val name: String
+}
+
+data class IngredientSavedEvent(val ingredient: Ingredient)
+    : ApplicationEvent(ingredient)
+
+class Ingredient(
+        private val record: IngredientRecord,
+        private val publisher: ApplicationEventPublisher)
+    : IngredientData by record {
+    fun save(): Ingredient {
+        record.flush()
+        publisher.publishEvent(IngredientSavedEvent(this))
+        return this
+    }
+}
 
 object IngredientRepository : IntIdTable("INGREDIENT") {
     val chef = reference("chef_id", ChefRepository)
@@ -12,10 +43,10 @@ object IngredientRepository : IntIdTable("INGREDIENT") {
     val sourceRef = reference("source_id", SourceRepository)
 }
 
-class IngredientRecord(id: EntityID<Int>) : IntEntity(id) {
+class IngredientRecord(id: EntityID<Int>) : IntEntity(id), IngredientData {
     companion object : IntEntityClass<IngredientRecord>(IngredientRepository)
 
-    val name
+    override val name
         get() = source.name
     var chef by ChefRecord referencedOn IngredientRepository.chef
     var recipe by RecipeRecord optionalReferencedOn IngredientRepository.recipe
