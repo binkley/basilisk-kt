c5ee58e2559e5bec11c5a94ada74914e0dae1f98
Brian Oxley <boxley@thoughtworks.com>
2019-07-16 17:43:20 -0500

    Test admin

--- src/test/kotlin/hm/binkley/basilisk/ApplicationTest.kt
+++ src/test/kotlin/hm/binkley/basilisk/ApplicationTest.kt
@@ -2,6 +2,9 @@ package hm.binkley.basilisk
 
 import ch.tutteli.atrium.api.cc.en_GB.toBe
 import ch.tutteli.atrium.verbs.expect
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.micronaut.http.HttpStatus.OK
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import java.net.URI
 import java.net.http.HttpClient.newHttpClient
 import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
 import java.net.http.HttpResponse.BodyHandlers.discarding
 import javax.inject.Inject
 
@@ -19,8 +23,39 @@ import javax.inject.Inject
 internal class ApplicationTest {
     @Inject
     lateinit var server: EmbeddedServer
+    @Inject
+    lateinit var objectMapper: ObjectMapper
 
     private val client = newHttpClient()
+
+    @Test
+    fun `should be healthy`() {
+        val request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:${server.port}/admin/health"))
+                .build();
+
+        val response = client.send(request, discarding())
+
+        expect(response.statusCode()).toBe(OK.code);
+    }
+
+    @Test
+    fun `should be informative`() {
+        val request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:${server.port}/admin/info"))
+                .build();
+
+        val response = client.send(request, BodyHandlers.ofByteArray())
+
+        expect(response.statusCode()).toBe(OK.code);
+
+        // TODO: Parsing fails on null nodes -- check this is true
+        objectMapper.readValue<Info>(response.body())
+    }
+
+    data class Info(val build: JsonNode, val git: JsonNode)
 
     @Test
     fun `should keep metrics`() {
