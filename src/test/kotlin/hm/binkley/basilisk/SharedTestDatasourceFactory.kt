f43e9646991e1fa57c56540889554ef2914c3ae5
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-07-21 07:31:32 -0500

    Proper cache

--- src/test/kotlin/hm/binkley/basilisk/SharedTestDatasourceFactory.kt
+++ src/test/kotlin/hm/binkley/basilisk/SharedTestDatasourceFactory.kt
@@ -7,6 +7,7 @@ import io.micronaut.context.annotation.Context
 import io.micronaut.context.annotation.EachBean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Replaces
+import java.util.concurrent.ConcurrentHashMap
 import javax.annotation.PreDestroy
 import javax.sql.DataSource
 
@@ -15,19 +16,19 @@ import javax.sql.DataSource
 class SharedTestDatasourceFactory(context: ApplicationContext)
     : DatasourceFactory(context) {
     companion object {
-        private var cached: DataSource? = null
+        private var cache = ConcurrentHashMap<String, DataSource>(1)
+
+        fun dataSource(jdbcUrl: String,
+                new: (jdbcUrl: String) -> DataSource) =
+                cache.computeIfAbsent(jdbcUrl, new)
     }
 
     @Context
     @EachBean(DatasourceConfiguration::class)
-    override fun dataSource(
-            datasourceConfiguration: DatasourceConfiguration?): DataSource {
-        if (null == cached) {
-            cached = super.dataSource(datasourceConfiguration)
-        }
-
-        return cached!!
-    }
+    override fun dataSource(configuration: DatasourceConfiguration?) =
+            dataSource(configuration!!.jdbcUrl) {
+                super.dataSource(configuration)
+            }
 
     @PreDestroy
     override fun close() {
