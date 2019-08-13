a3877a9587a9d85b7beb635d8c74ddb50485f4ab
Brian Oxley <boxley@thoughtworks.com>
2019-08-13 04:53:17 -0500

    Formatting

--- recipe-lib/src/main/kotlin/hm/binkley/basilisk/recipe/RecipeLocations.kt
+++ recipe-lib/src/main/kotlin/hm/binkley/basilisk/recipe/RecipeLocations.kt
@@ -4,8 +4,8 @@ import hm.binkley.basilisk.location.LocationRepository
 import org.jetbrains.exposed.sql.Table
 
 object RecipeLocationsRepository : Table("RECIPE_LOCATION") {
-    val recipe = reference("recipe_id", RecipeRepository)
-            .primaryKey(0)
-    val location = reference("location_id", LocationRepository)
-            .primaryKey(1)
+    val recipe =
+            reference("recipe_id", RecipeRepository).primaryKey(0)
+    val location =
+            reference("location_id", LocationRepository).primaryKey(1)
 }
--- source-lib/src/main/kotlin/hm/binkley/basilisk/source/SourceLocations.kt
+++ source-lib/src/main/kotlin/hm/binkley/basilisk/source/SourceLocations.kt
@@ -5,8 +5,8 @@ import org.jetbrains.exposed.sql.Table
 
 object SourceLocationsRepository : Table("SOURCE_LOCATION") {
     // TODO: The hierarchy of `Table` defines a visible "source" field
-    val sourceRef = reference("source_id", SourceRepository)
-            .primaryKey(0)
-    val location = reference("location_id", LocationRepository)
-            .primaryKey(1)
+    val sourceRef =
+            reference("source_id", SourceRepository).primaryKey(0)
+    val location =
+            reference("location_id", LocationRepository).primaryKey(1)
 }
