aa176683d2696de9b37d9c952f4deb7ec73a028a
Brian Oxley <boxley@thoughtworks.com>
2019-08-18 05:56:15 -0500

    Run db only; make shellcheck happier

--- run-basilisk.sh
+++ run-basilisk.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC2214,SC2215
+# shellcheck disable=SC2214
 
 export PS4='+${BASH_SOURCE}:${LINENO}:${FUNCNAME[0]:+${FUNCNAME[0]}():} '
 
@@ -14,26 +14,31 @@ printf -v pred "\e[31m"
 printf -v pgreen "\e[32m"
 printf -v pyellow "\e[33m"
 
-function -print-usage() {
+function _print-usage() {
   echo "Usage: $0 [-h|--help] [TARGETS(s)]"
 }
 
-function -print-help() {
-  -print-usage
+function _print-help() {
+  _print-usage
   cat <<EOH
 
 Options:
-  -e, --environment=ENVIRONMENT(s)
+  -E, --environments=ENVIRONMENT(s)
                   Run with specific Micronaut ENVIRONMENT(s), comma-separated
                     if multiple; default is "app db"
   -h, --help      Print this help and exit
   -n, --dry-run   Prints what would execute, but does not execute
 
 Arguments:
-  TARGETS(s)  Run specific TARGET(s); default is "basil chefs".
+  TARGETS(s)  Run specific TARGET(s); default is "basil chefs".  All targets
+    include tailing logs.  Top-level targets:
+      - chefs   Runs chefs service
+      - basil   Runs basil service
+      - db      Runs just database
 EOH
 }
 
+# We always need these to have a functioning program
 basil_environments=("app" "db")
 chefs_environments=("app" "db")
 
@@ -41,19 +46,20 @@ run=
 while getopts :E:e:hn-: opt; do
   [[ $opt == - ]] && opt=${OPTARG%%=*} OPTARG=${OPTARG#*=}
   case $opt in
-  E | environment)
-    IFS=, read -r -a basil_environments <<<"$OPTARG"
-    IFS=, read -r -a chefs_environments <<<"$OPTARG"
+  E | environments)
+    IFS=, read -r -a all_environments <<<"$OPTARG"
+    basil_environments=("${basil_environments[@]}" "${all_environments[@]}")
+    chefs_environments=("${chefs_environments[@]}" "${all_environments[@]}")
     ;;
   h | help)
-    -print-help
+    _print-help
     exit 0
     ;;
   n | dry-run)
     run="echo"
     ;;
   *)
-    -print-usage >&2
+    _print-usage >&2
     exit 2
     ;;
   esac
@@ -77,6 +83,8 @@ tmpdir="$(mktemp -d 2>/dev/null || mktemp -d -t basilisk)"
 function stack-trace-and-exit() {
   local err=$?
   set +o xtrace # Do not trace this code
+  # TODO: Fix the exit code
+  # shellcheck disable=SC2034
   local code="${1:-1}"
   echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]}. '${BASH_COMMAND}' exited with status $err"
   # Print out the stack trace described by $function_stack
@@ -92,17 +100,17 @@ function stack-trace-and-exit() {
 trap stack-trace-and-exit ERR
 set -o errtrace
 
-function -join() {
+function _join() {
   local IFS=,
   echo "$*"
 }
 
 logs_to_tail=()
-function -tail-log() {
+function _tail-log() {
   logs_to_tail=("${logs_to_tail[@]}" "$1")
 }
 
-function -ready-or-die() {
+function _ready-or-die() {
   local bgpid=$1
   local out_file=$2
   local ready_text=$3
@@ -111,9 +119,9 @@ function -ready-or-die() {
   while ! grep "$ready_text" "$out_file" >/dev/null; do
     # `kill -0 <pid>` is neat: Check that the process is running, but do not
     # actually delivery a signal
-    kill -0 $bgpid 2>/dev/null || {
+    kill -0 "$bgpid" 2>/dev/null || {
       # Meaning ... wait for fake signal to be delivered
-      wait $bgpid || {
+      wait "$bgpid" || {
         rc=$?
         cat "$out_file"
         exit $rc
@@ -138,7 +146,8 @@ function reset-docker() {
 function run-postgres() {
   echo "Waiting for postgres to be ready ..."
   docker-compose up basilisk-db >>"$tmpdir/postgres" 2>&1 &
-  -ready-or-die $! "$tmpdir/postgres" 'database system is ready to accept connections'
+  _ready-or-die $! "$tmpdir/postgres" 'database system is ready to accept connections'
+  _tail-log "$tmpdir/postgres"
 }
 
 function install-schemas() {
@@ -167,15 +176,15 @@ function build-all() {
 function run-chefs() {
   echo "Waiting for Chefs ..."
   local chefs_vm_args=("${chefs_vm_args[@]}"
-    "-Dmicronaut.environments=$(-join "${chefs_environments[@]}" "$@")")
+    "-Dmicronaut.environments=$(_join "${chefs_environments[@]}" "$@")")
   echo "Running java ${chefs_vm_args[*]} -jar chefs-bin/build/libs/*-all.jar" >"$tmpdir/chefs"
   java "${chefs_vm_args[@]}" -jar chefs-bin/build/libs/*-all.jar >>"$tmpdir/chefs" 2>&1 &
-  -ready-or-die $! "$tmpdir/chefs" "Startup completed in"
-  -tail-log "$tmpdir/chefs"
+  _ready-or-die $! "$tmpdir/chefs" "Startup completed in"
+  _tail-log "$tmpdir/chefs"
 }
 
-function -mock-service() {
-  [[ -n "$(which socat 2>/dev/null)" ]] || {
+function _mock-service() {
+  command -v socat >/dev/null || {
     echo "${pred}$0: ERROR: 'socat' not installed${preset}" >&2
     exit 1
   }
@@ -184,29 +193,29 @@ function -mock-service() {
   local exec="$2"
   local log="$3"
 
-  socat -v tcp-l:$port,fork exec:$exec >"$log" 2>&1 &
+  socat -v tcp-l:"$port",fork exec:"$exec" >"$log" 2>&1 &
 }
 
 function mock-chefs() {
   echo "Mocking Chefs ..."
-  -mock-service 7372 ./mock-chefs.sh "$tmpdir/chefs"
-  -tail-log "$tmpdir/chefs"
+  _mock-service 7372 ./mock-chefs.sh "$tmpdir/chefs"
+  _tail-log "$tmpdir/chefs"
 }
 
 function run-basil() {
   echo "Waiting for Basil ..."
   local basil_vm_args=("${basil_vm_args[@]}"
-    "-Dmicronaut.environments=$(-join "${basil_environments[@]}" "$@")")
+    "-Dmicronaut.environments=$(_join "${basil_environments[@]}" "$@")")
   echo "Running java ${basil_vm_args[*]} -jar basil-bin/build/libs/*-all.jar" >"$tmpdir/basil"
   java "${basil_vm_args[@]}" -jar basil-bin/build/libs/*-all.jar >>"$tmpdir/basil" 2>&1 &
-  -ready-or-die $! "$tmpdir/basil" "Startup completed in"
-  -tail-log "$tmpdir/basil"
+  _ready-or-die $! "$tmpdir/basil" "Startup completed in"
+  _tail-log "$tmpdir/basil"
 }
 
 function mock-basil() {
   echo "Mocking Basil ..."
-  -mock-service 7371 ./mock-basil.sh "$tmpdir/basil"
-  -tail-log "$tmpdir/basil"
+  _mock-service 7371 ./mock-basil.sh "$tmpdir/basil"
+  _tail-log "$tmpdir/basil"
 }
 
 function tail-logs() {
@@ -226,11 +235,14 @@ basil: need-basil
 else
 basil: mock-basil
 endif
+
 ifneq ($(filter-out chefs,$(MAKECMDGOALS)),$(MAKECMDGOALS))
 chefs: need-chefs
 else
 chefs: mock-chefs
 endif
+
+db: need-schemas
 logs: need-logs
 
 need-docker:
