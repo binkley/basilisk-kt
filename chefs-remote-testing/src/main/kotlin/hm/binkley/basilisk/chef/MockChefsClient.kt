b3cfe32d2b255452e1798723c39493fb47fe01dc
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-08-23 16:49:09 -0500

    Nicer code

--- chefs-remote-testing/src/main/kotlin/hm/binkley/basilisk/chef/MockChefsClient.kt
+++ chefs-remote-testing/src/main/kotlin/hm/binkley/basilisk/chef/MockChefsClient.kt
@@ -8,7 +8,7 @@ import javax.inject.Singleton
 @Replaces(ChefsClient::class)
 @Singleton
 class MockChefsClient : ChefsClient {
-    var all = listOf(ChefResource("NOT SET", "NOT SET"))
+    var all = emptyList<ChefResource>()
     var one: ChefResource? = null
 
     fun reset() {
