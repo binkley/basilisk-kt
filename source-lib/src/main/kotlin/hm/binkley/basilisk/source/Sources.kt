f45a616342f656b2d4323d7f375b2913e3167165
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2019-09-02 08:53:59 -0500

    Formatting

--- source-lib/src/main/kotlin/hm/binkley/basilisk/source/Sources.kt
+++ source-lib/src/main/kotlin/hm/binkley/basilisk/source/Sources.kt
@@ -92,9 +92,7 @@ class Source internal constructor(
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
-
         other as Source
-
         return record == other.record
     }
 
